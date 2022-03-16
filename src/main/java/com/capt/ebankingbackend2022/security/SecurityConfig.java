7c1239a8228d9090d653b59d46dbce6460626982
Nguyen Tran <nguyentran.andrd@gmail.com>
2022-03-16 10:16:47 +0700

    make it better

--- src/main/java/com/capt/ebankingbackend2022/security/SecurityConfig.java
+++ src/main/java/com/capt/ebankingbackend2022/security/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -55,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // Optional, if you want to test the API from a browser
 //        http.httpBasic();
     }
+
 
 
 }
