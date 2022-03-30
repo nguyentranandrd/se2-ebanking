0f9edb4fc83adedc871966263ad867c28da1b747
Nguyen Tran <nguyentran.andrd@gmail.com>
2022-03-30 14:20:17 +0700

    update code

--- src/main/java/com/capt/ebankingbackend2022/controller/AuthController.java
+++ src/main/java/com/capt/ebankingbackend2022/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.capt.ebankingbackend2022.controller;
 import com.capt.ebankingbackend2022.dto.AccountDto;
 import com.capt.ebankingbackend2022.dto.LoginRequestDto;
 import com.capt.ebankingbackend2022.dto.RegisterAccountDto;
+import com.capt.ebankingbackend2022.entity.CodeEntity;
+import com.capt.ebankingbackend2022.repository.CodeRepository;
 import com.capt.ebankingbackend2022.service.AuthService;
 import com.capt.ebankingbackend2022.utils.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,17 +13,36 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    CodeRepository codeRepository;
+
 
     @GetMapping("/ping")
     public Response<String> pingServer() {
         return new Response<>(0, "ping success");
     }
+
+
+    @GetMapping("/code")
+    public Response<CodeEntity> newAdminCode() {
+        String code = UUID.randomUUID().toString();
+        CodeEntity codeEntity = new CodeEntity();
+        codeEntity.setCode(code);
+        codeEntity.setActive(true);
+        codeEntity.setCreatedAt(new Date());
+        codeRepository.save(codeEntity);
+        return new Response<>(0, "success", codeEntity);
+    }
+
 
     @PostMapping("/login")
     public ResponseEntity<Response<String>> login(@RequestBody LoginRequestDto loginRequestDto) {
